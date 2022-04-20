b768c8917865ab7ed4c9c30be096c00b6e55bffc
kelvinkip <kelvinkipla@gmail.com>
2022-04-20 19:07:52 +0300

    Auto-generated commit [ci skip]

--- src/app/username.spec.ts
+++
@@ -1,7 +0,0 @@
-// import { Username } from './username';
-
-// describe('Username', () => {
-//   it('should create an instance', () => {
-//     expect(new Username()).toBeTruthy();
-//   });
-// });
